3e671f34f16938ddac151f8940f74eb5ee6206c1
aze-cyper <3156455728@qq.com>
2025-11-09 12:22:40 +0800

    1

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function show_developer(){
-    document.getElementById("develop").innerHTML="伟大的、性感的、传奇的aze"
+    document.getElementById("develop").innerHTML="无敌的、勇猛的、性感的、神秘的、迷人的、神气的、勤勉的、强势的、华丽的、激情的、可怕的、漂亮的、强大的aze"
 }
